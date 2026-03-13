4ead1874fd573ddafd226ec935a769a33cc7b7e3
jeferssonp88 <jeferssonp88@gmail.com>
2026-03-12 23:34:18 -0400

    Update ares.py

--- ares.py
+++ ares.py
@@ -1,13 +1,12 @@
-
 import telebot
 
 TOKEN = '8607330231:AAFcQFP1qB0ItflRC2vevWJYuEH-Q0oBwiw'
-MI_ID = 6523912194  # <--- Aquí es donde usamos tu ID
+MI_ID = 6523912194
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda m: m.chat.id == MI_ID)
 def respuesta(m):
-    bot.reply_to(m, "🦾 ARES: Te escucho fuerte y claro, jefe.")
+    bot.reply_to(m, "🦾 ARES: Te escucho fuerte y claro, jefe. Operativo en la nube.")
 
 print(">>> ARES ESTA VIVO Y TE RECONOCE")
 bot.infinity_polling()
